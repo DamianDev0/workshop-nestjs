6335784b4a399acccd0114807ee2d1e23dd9dbce
damian <dg997678@gmail.com>
2024-08-14 19:17:22 -0500

    update chane in auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,35 +1,13 @@
-import {
-  Controller,
-  Post,
-  Body,
-  UseGuards,
-  HttpCode,
-  HttpStatus,
-} from '@nestjs/common';
-import { JwtAuthGuard } from './jwt-auth.guard'; // Ajusta la ruta según la ubicación real de tu guard
-import { LoansService } from '@src/loans/loans.service';
-import { CreateLoanDto } from '@src/loans/dto/create-loan.dto';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateAuthDto } from './dto/create-auth.dto';
 
-@Controller('loans')
-export class LoansController {
-  constructor(private readonly loansService: LoansService) {}
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
 
-  @Post()
-  @UseGuards(JwtAuthGuard)
-  @HttpCode(HttpStatus.CREATED)
-  async create(@Body() createLoanDto: CreateLoanDto) {
-    try {
-      const newLoan = await this.loansService.create(createLoanDto);
-      return {
-        status: HttpStatus.CREATED,
-        message: 'Loan created successfully',
-        data: newLoan,
-      };
-    } catch (error) {
-      return {
-        status: HttpStatus.BAD_REQUEST,
-        message: 'Error creating loan',
-      };
-    }
+  @Post('login')
+  async login(@Body() createAuthDto: CreateAuthDto) {
+    return this.authService.login(createAuthDto);
   }
 }
